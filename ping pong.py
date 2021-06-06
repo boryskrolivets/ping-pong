25f8a74ce21c587b0a4697a1d90b798e48cbce6d
boryskrolivets <85443370+boryskrolivets@users.noreply.github.com>
2021-06-06 17:22:42 +0300

    Add files via upload

---
+++ ping pong.py
@@ -0,0 +1,13 @@
+from pygame import*
+win_height = 700
+win_width=500
+window = display.set_mode(win_width,win_height)
+display.set_caption("Пинг-понг")
+clock = time.clock()
+FPS = 60
+class GameSprite(sprite.Sprite):
+    
+
+
+
+
